dc5550f2ed10ba4725eb89df655d06291884194f
Eric <ericso0303@gmail.com>
2016-08-28 21:13:31 -0700

    [Created] Object inheritance using utils

---
+++ inheritance/app.js
@@ -0,0 +1,21 @@
+var Events = require('events');
+
+var util = require('util');
+
+function Greetr(){
+	this.greeting = 'Hello World';
+}
+
+util.inherits(Greetr, Events);
+
+Greetr.prototype.greet = function(data){
+	console.log("greeting...", this.greeting);
+	this.emit('greet', data);
+}
+var greetr1 = new Greetr();
+
+greetr1.on('greet', function(data){
+	console.log("someone greeted!" + data);
+})
+
+greetr1.greet('Ericc!!');
