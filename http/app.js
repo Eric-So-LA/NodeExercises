dadc1514805a3fe8ee0c5df2bad097d69c274e93
Eric <ericso0303@gmail.com>
2016-09-01 19:05:19 -0700

    [Feature] Routing

--- http/app.js
+++ http/app.js
@@ -3,14 +3,19 @@ var fs = require('fs');
 
 http.createServer(function(req, res){
 
-	res.writeHead(200, { 'Content-Type': 'application/json' });
+	if(req.url === '/'){
+		fs.createReadStream(__dirname + '/index.htm').pipe(res);
+	} else if(req.url === '/api'){
+		res.writeHead(200, { 'Content-Type': 'application/json' });
 
-	var obj = {
-		firstname: 'John',
-		lastname: 'Doe'
-	}
-	res.end(JSON.stringify(obj));
+		var obj = {
+				firstname: 'John',
+				lastname: 'Doe'
+			}
+		res.end(JSON.stringify(obj));
+	} else {
 	// fs.createReadStream(__dirname + '/index.htm').pipe(res);	
-
-
+		res.writeHead(404);
+		res.end();
+	}
 }).listen(1337, '127.0.0.1');
