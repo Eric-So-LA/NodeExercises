ba789d15347b7e212a1d0588072c4ee5c80a701b
Eric <ericso0303@gmail.com>
2016-09-05 21:58:25 -0700

    [Learned] Manipulating parameters in url with express

--- express/app.js
+++ express/app.js
@@ -2,13 +2,23 @@ var express = require('express');
 
 var app = express();
 
-
 var port = process.env.PORT || 3000;
-app.get('/', function(req, res){
+app.use('/assets', express.static(__dirname + '/public'));
 
-	res.send('<html><head></head><body><h1>hello!!!</h1></body></html>')
+app.set('view engine', 'ejs');
+app.use(function(req, res, next){
+	console.log("this is url", req.url);
+
+	next();
+})
+app.get('/', function(req, res){
+	res.render('index');
 })
 
+app.get('/person/:id', function(req, res){
+
+	res.render('person', {ID: req.params.id});
+})
 app.get('/api/:id', function(req, res){
 	res.send('<html><head></head><body><h1>hello'+ req.params.id + '</h1></body></html>')
 
